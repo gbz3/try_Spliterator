9e45342f3a2839c217d574280ac57d207a068a74
gbz3 <1808483+gbz3@users.noreply.github.com>
2024-12-28 10:12:02 +0900

    dummy test

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -6,16 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
 
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    private String input;
+    private String output;
+
+    @Given("可変長レコード is {string}")
+    public void anExampleScenario(String input) {
+        this.input = input;
     }
 
     @When("all step definitions are implemented")
     public void allStepDefinitionsAreImplemented() {
     }
 
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
+    @Then("変換結果 is {string}")
+    public void theScenarioPasses(String output) {
     }
 
 }
--- hellocucumber/target/test-classes/hellocucumber/example.feature
+++ hellocucumber/target/test-classes/hellocucumber/example.feature
@@ -1,6 +1,10 @@
-Feature: An example
+Feature: 可変長レコードを変換する
 
-  Scenario: The example
-    Given an example scenario
+  Scenario Outline: The example
+    Given 可変長レコード is "<input>"
     When all step definitions are implemented
-    Then the scenario passes
+    Then 変換結果 is "<output>"
+
+  Examples:
+    | input | output |
+    | A003AAAB002BB | AAA:BB |
